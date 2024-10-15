06428dfbed0245d01436a4b574b97ef40d65548a
Viclop34 <vl48011@gmail.com>
2024-10-15 17:37:07 -0600

    ya po favo

--- Cinepolis/src/salas/Salas.java
+++ Cinepolis/src/salas/Salas.java
@@ -1,6 +1,7 @@
 package salas;
 
 import asientos.Asientos;
+import peliculas.gestionPelicullas.Proyeccion;
 import resources.Rol;
 
 import java.time.LocalDate;
@@ -8,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Salas {
+
+    ArrayList<Proyeccion> listaProyecciones= new ArrayList<>();
     String idSalas;
     int capacidadAsientos;
+    List<Asientos> listaAsientos = new ArrayList<Asientos>();
     Asientos tipoAsiento;
     LocalDate horaDeFuncion;
     int cantidadAsientosVIP;
@@ -19,20 +23,24 @@ public class Salas {
     public Salas(String id, int capacidadAsientos, ArrayList listaAsientos, Asientos tipoAsiento, LocalDate horaDeFuncion, int cantidadAsientosVIP, int cantidadAsientosPremium, String pelicula) {
         this.idSalas = id;
         this.capacidadAsientos = capacidadAsientos;
+        this.listaAsientos = listaAsientos;
         this.tipoAsiento = tipoAsiento;
         this.horaDeFuncion = horaDeFuncion;
         this.cantidadAsientosVIP = cantidadAsientosVIP;
         this.cantidadAsientosPremium = cantidadAsientosPremium;
         this.pelicula = pelicula;
+        this.listaProyecciones = new ArrayList<>();
     }
 
     //Getters
-    public String getId() {
-        return idSalas;
-    }
+    public String getIdSalas() {return idSalas;}
 
     public int getCapacidadAsientos() {
         return capacidadAsientos;
+    }
+
+    public List<Asientos> getListaAsientos() {
+        return listaAsientos;
     }
 
     public Asientos getTipoAsiento() {
@@ -55,6 +63,9 @@ public class Salas {
         return pelicula;
     }
 
+    public ArrayList<Proyeccion> getProyecciones() {return listaProyecciones;}
+
+
     //Setters
     public void setId(String id) {
         this.idSalas = id;
@@ -62,6 +73,10 @@ public class Salas {
 
     public void setCapacidadAsientos(int capacidadAsientos) {
         this.capacidadAsientos = capacidadAsientos;
+    }
+
+    public void setListaAsientos(List<Asientos> listaAsientos) {
+        this.listaAsientos = listaAsientos;
     }
 
     public void setTipoAsiento(Asientos tipoAsiento) {
@@ -82,5 +97,14 @@ public class Salas {
 
     public void setPelicula(String pelicula) {
         this.pelicula = pelicula;
+    }
+
+    public String mostrarDatosSalas(){
+        String datosSalas = String.format("ID: %s, Capacidad de asientos: %s, Lista Asientos: %s, Tipo de asiento: %s, Hora de función: %s, Cantidad Asientos VIP: %d, Cantidad Asientos Premium: %d", idSalas, capacidadAsientos, listaAsientos, tipoAsiento, horaDeFuncion,cantidadAsientosVIP,cantidadAsientosPremium );
+        return datosSalas;
+    }
+
+    public void agregarProyeccion(Proyeccion proyeccion) {
+        listaProyecciones.add(proyeccion);
     }
 }
